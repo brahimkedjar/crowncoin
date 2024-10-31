32c4374f04ec74585773c8c48d4caa31fa732488
brahim kedjar <jemskedjar@gmail.com>
2024-10-31 21:46:27 +0100

    Add serviceAccountKey.json to .gitignore82652555

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import MaintenancePage from './maintenance';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      /<MaintenancePage />
+      /<App />
     </Router>
   </React.StrictMode>
 );
